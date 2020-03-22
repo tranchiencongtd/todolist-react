29756cbd393680b30c09e490ec40a8cbf81f3727
congngusi <tranchiencongtd@gmail.com>
2020-03-22 21:33:38 +0700

    finish todo-list

--- app.js
+++ app.js
@@ -47,6 +47,6 @@ app.get('/', function(req, res) {
   });
 });
 
-app.listen('3000', function (){
+app.listen(process.env.PORT || 3000, function (){
   console.log("Server turning on port 3000");
 });
